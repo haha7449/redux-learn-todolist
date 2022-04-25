990d1dca5e0c4de4b0051a6d3dfad62a0d9946ee
hah7449 <532298135@qq.com>
2022-04-25 15:31:24 +0800

    change readme.md

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import reducer from "./reducer";
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()//redux扩展程序
 );
 
 export default store;
